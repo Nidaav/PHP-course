20a4a609a816889febfa2f0b48d0f0821837bc3d
leo tanguy <leo0703@hotmail.fr>
2022-05-12 17:28:20 +0200

    20%

--- jeu.php
+++ jeu.php
@@ -1,17 +1,30 @@
 <?php
 $aDeviner = 150;
+$erreur = null;
+$succes = null;
+if (isset($_GET['chiffre'])) {
+  if ($_GET['chiffre'] > $aDeviner) {
+    $erreur = "Votre chiffre est trop grand";
+  } elseif ($_GET['chiffre'] < $aDeviner) {
+    $erreur = "Votre chiffre est trop petit";
+  } else {
+    $succes = "Bravo ! Vous avez deviné le chiffre <strong>$aDeviner</strong>";
+  }
+  $value = (int)$_GET['chiffre'];
+}
 ?>
-<?php if (isset($_GET['chiffre'])): ?>
-  <?php if ($_GET['chiffre'] > $aDeviner): ?>   //On va directement chercher dans l'url de la page ce que l'utilisateur a rentré dans le formulaire
-    Votre chiffre est trop grand
-  <?php elseif ($_GET['chiffre'] < $aDeviner): ?>
-    Votre chiffre est trop petit
-  <?php else: ?>
-    Bravo ! Vous avez deviné le chiffre <?= $aDeviner ?>
-  <?php endif ?>
+<?php if ($erreur): ?>
+  <div class="alert alert-danger">
+    <?= $erreur ?>
+  </div>
+<?php elseif ($succes): ?>
+  <div class="alert alert-success">
+    <?= $succes ?>
+  </div>
 <?php endif ?>
 
 <form action="/jeu.php" method="GET">
-  <input type="number" name="chiffre" placeholder="Entre 0 et 1000" value="<?php if (isset($_GET['chiffre'])) { echo htmlentities($_GET['chiffre']); } ?>">
+  <input type="number" name="chiffre" placeholder="Entre 0 et 1000" value="<?= $value ?>">
+  <input type="text" name="demo" value="test">
   <button type="submit">Devinez</button>
 </form>
